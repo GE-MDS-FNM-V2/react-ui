14607bb55534f6c72a589191024aa6ad66957d33
Brady Brown <bobrown101@gmail.com>
2020-03-29 16:37:08 -0400

    add crude search box if youre in electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 
-function App() {
-  return <Home />;
-}
+const searchHeight = '50px';
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const inElectron = window.require ? true : false;
+
+  const submit = (e: React.SyntheticEvent) => {
+    if (inElectron) {
+      const electron = window.require('electron');
+      e.preventDefault();
+      electron.webFrame.context.find(inputText);
+    }
+  };
+
+  return (
+    <div>
+      {inElectron && (
+        <div
+          className="search-box"
+          style={{
+            position: 'fixed',
+            top: '0',
+            right: '0',
+            height: searchHeight,
+            zIndex: 100,
+            background: 'white',
+            borderRadius: '5px',
+            padding: '10px',
+            border: '1px solid black'
+          }}
+        >
+          <form>
+            <div>
+              <input
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                placeholder="Search text here"
+              />
+              <button onClick={submit}>Search</button>
+            </div>
+          </form>
+        </div>
+      )}
+      <div style={{ marginTop: inElectron ? searchHeight : '' }}>
+        <Home />
+      </div>
+    </div>
+  );
+};
 
 export default App;
